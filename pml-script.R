e96cec0bf83b4ec48b3ce71736a70c6a03968de9
Peter Setter <peter.w.setter@gmail.com>
2015-08-22 16:22:50 -0600

    Commented script

--- pml-script.R
+++ pml-script.R
@@ -12,8 +12,25 @@
 ## Library Calls
 library(dplyr)
 library(caret)
+##############
+## Function Definitions
+CV <- function(x){
+  ## Calculate the coefficient of variance
+  ## Args:
+  ##  x: a numeric vector
+  ## Returns:
+  ##  coeffcient of variance
+  return(abs((sd(x)/mean(x))))
+}
 
+## Define unary function to filter rows that contain summary statistics
+## and selects the columns containing those summary statistics
+SumStats <- . %>% filter(new_window == 'yes') %>% ungroup %>%
+  select(classe, contains("max"), contains("min"), contains("kurtosis"),
+         contains("skewness"), contains("amplitude"), contains("avg"),
+         contains("var"))
 
+############
 ## Download the training data
 train.url <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
 train.file <- "train.csv"
@@ -35,43 +52,41 @@ quiz <- train.all[-train$X,]
 ## The creators of the data set calculated numerous summary statistics that
 ## appear in rows with new_window = 'yes'
 
-## Define unary function to filter & select
-SumStats <- . %>% filter(new_window == 'yes') %>% ungroup %>%
-  select(classe, contains("max"), contains("min"), contains("kurtosis"),
-         contains("skewness"), contains("amplitude"), contains("avg"),
-         contains("var"))
-
 train.ss <- SumStats(train)
 quiz.ss <- SumStats(quiz)
 
-CV <- function(x){
-  return(abs((sd(x)/mean(x))))
-}
+## Find the coeffiecient of variance for the variables.
 train.CV <- summarise_each(train.ss, funs(CV))
 
 train.ss.col <- ncol(train.ss)
 
+## Select (by elimination) columns where the CV is NA or <= 1 
 for (i in train.ss.col:2) {
   if(is.na(train.CV[1,i]) | train.CV[1, i] <= 1) {
     train.ss [i] <- NULL
   }
 }
+##########
+##Train models
 
+## CART
 set.seed(2015)
 ss.model.rpart <- train(classe ~ ., data = train.ss, method = "rpart")
 
 prdct.ss.rpart <- predict(ss.model.rpart, newdata = quiz.ss)
 
-confusionMatrix(data = prdct.ss.rpart, quiz.ss$classe)
+cm.rpart <- confusionMatrix(data = prdct.ss.rpart, quiz.ss$classe)
 
+## Random forest (randomForest package)
 ss.model.rf <- train(classe ~ ., data = train.ss, method = "rf")
 
 prdct.ss.rf <- predict(ss.model.rf, newdata = quiz.ss)
 
-confusionMatrix(data = prdct.ss.rf, quiz.ss$classe)
+cm.rf <- confusionMatrix(data = prdct.ss.rf, quiz.ss$classe)
 
+## Stochastic Gradient Boosting
 ss.model.gbm <- train(classe ~ ., data = train.ss, method = "gbm")
 
 prdct.ss.gbm <- predict(ss.model.gbm, newdata = quiz.ss)
 
-confusionMatrix(data = prdct.ss.gbm, quiz.ss$classe)$overall[1]
+cm.gbm <- confusionMatrix(data = prdct.ss.gbm, quiz.ss$classe)
