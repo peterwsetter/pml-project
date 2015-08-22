4e2ababaabb95668dd563ade9e2a7ea76de491d9
Peter Setter <peter.w.setter@gmail.com>
2015-08-22 14:47:57 -0600

    Data Cleaning and Model Testing

--- pml-script.R
+++ pml-script.R
@@ -20,7 +20,15 @@ train.file <- "train.csv"
 if (!exists("train.csv")) {
   download.file(train.url, train.file, method = "curl")
 }
-train.all <- read.csv(train.file)
+## Data set contains #DIV/0! for some of its variables
+## Set #DIV/0! to NA
+train.all <- read.csv(train.file, na.strings = c("NA", "#DIV/0!"))
+
+## Divide the training data into a true training set and a "quiz" set
+## "classe" is the A-E description
+## "num_window" identifies the particular repitition of the exercise
+train <- train.all %>% group_by(num_window, classe) %>% sample_frac(0.7)
+quiz <- train.all[-train$X,]
 
 ###############
 ## Filter and select summary statistics
@@ -28,7 +36,7 @@ train.all <- read.csv(train.file)
 ## appear in rows with new_window = 'yes'
 
 ## Define unary function to filter & select
-SumStats <- . %>% filter(new_window == 'yes') %>% 
+SumStats <- . %>% filter(new_window == 'yes') %>% ungroup %>%
   select(classe, contains("max"), contains("min"), contains("kurtosis"),
          contains("skewness"), contains("amplitude"), contains("avg"),
          contains("var"))
@@ -36,4 +44,34 @@ SumStats <- . %>% filter(new_window == 'yes') %>%
 train.ss <- SumStats(train)
 quiz.ss <- SumStats(quiz)
 
-ss.model <- train(classe ~ ., data = train.ss[-1,], method = "nb")
+CV <- function(x){
+  return(abs((sd(x)/mean(x))))
+}
+train.CV <- summarise_each(train.ss, funs(CV))
+
+train.ss.col <- ncol(train.ss)
+
+for (i in train.ss.col:2) {
+  if(is.na(train.CV[1,i]) | train.CV[1, i] <= 1) {
+    train.ss [i] <- NULL
+  }
+}
+
+set.seed(2015)
+ss.model.rpart <- train(classe ~ ., data = train.ss, method = "rpart")
+
+prdct.ss.rpart <- predict(ss.model.rpart, newdata = quiz.ss)
+
+confusionMatrix(data = prdct.ss.rpart, quiz.ss$classe)
+
+ss.model.rf <- train(classe ~ ., data = train.ss, method = "rf")
+
+prdct.ss.rf <- predict(ss.model.rf, newdata = quiz.ss)
+
+confusionMatrix(data = prdct.ss.rf, quiz.ss$classe)
+
+ss.model.gbm <- train(classe ~ ., data = train.ss, method = "gbm")
+
+prdct.ss.gbm <- predict(ss.model.gbm, newdata = quiz.ss)
+
+confusionMatrix(data = prdct.ss.gbm, quiz.ss$classe)$overall[1]
