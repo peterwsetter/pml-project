0f3a83667d25aca0374fbb578d8682f3bccca1c8
Peter Setter <peter.w.setter@gmail.com>
2015-08-23 09:28:48 -0600

    Alt route, since no summary statistics are available

--- pml-script.R
+++ pml-script.R
@@ -23,69 +23,95 @@ CV <- function(x){
   return(abs((sd(x)/mean(x))))
 }
 
-## Define unary function to filter rows that contain summary statistics
-## and selects the columns containing those summary statistics
-SumStats <- . %>% filter(new_window == 'yes') %>% ungroup %>%
-  select(classe, contains("max"), contains("min"), contains("kurtosis"),
-         contains("skewness"), contains("amplitude"), contains("avg"),
-         contains("var"))
+## Define unary function to remove rows that contain summary statistics
+## since these do not appear in the testing data set
+Filtered <- . %>% filter(new_window == 'no') %>% ungroup %>%
+  select(-X, -user_name, -contains("time"), -contains("window"), 
+         -contains("max"), -contains("min"), -contains("kurtosis"),
+         -contains("skewness"), -contains("amplitude"), -contains("avg"),
+         -contains("var"), -contains("stddev"))
 
+## Unary function for training a model using train.filtered
+## Argument is the method 
+control.spec <- trainControl(method = 'cv', number = 10)
+
+TrainClasse <- . %>% train(classe ~ ., 
+                           data = train.filtered, 
+                           trControl = control.spec,
+                           method = .,
+                           verbose = FALSE)
+
+
+pml_write_files = function(x){
+  ## Writes files for submission to the PML course website
+  ## Args:
+  ##  x: Character vector
+  ## Returns
+  ##  A text file containing the character in each index of the vector
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
 ############
 ## Download the training data
 train.url <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
 train.file <- "train.csv"
-if (!exists("train.csv")) {
+if (!file.exists("train.csv")) {
   download.file(train.url, train.file, method = "curl")
 }
 ## Data set contains #DIV/0! for some of its variables
 ## Set #DIV/0! to NA
-train.all <- read.csv(train.file, na.strings = c("NA", "#DIV/0!"))
+train.full <- read.csv(train.file, na.strings = c("NA", "#DIV/0!"))
 
 ## Divide the training data into a true training set and a "quiz" set
 ## "classe" is the A-E description
-## "num_window" identifies the particular repitition of the exercise
-train <- train.all %>% group_by(num_window, classe) %>% sample_frac(0.7)
-quiz <- train.all[-train$X,]
+train <- train.full %>% group_by(classe) %>% sample_frac(0.7)
+quiz <- train.full[-train$X,]
 
 ###############
 ## Filter and select summary statistics
 ## The creators of the data set calculated numerous summary statistics that
 ## appear in rows with new_window = 'yes'
 
-train.ss <- SumStats(train)
-quiz.ss <- SumStats(quiz)
+train.filtered <- Filtered(train)
+quiz.filtered <- Filtered(quiz)
 
+#########
 ## Find the coeffiecient of variance for the variables.
-train.CV <- summarise_each(train.ss, funs(CV))
+train.CV <- summarise_each(train.filtered, funs(CV))
+train.col <- ncol(train.filtered) -1
+avg.CV <- rowMeans(train.CV[-train.col], na.rm = TRUE)
 
-train.ss.col <- ncol(train.ss)
-
-## Select (by elimination) columns where the CV is NA or <= 1 
-for (i in train.ss.col:2) {
-  if(is.na(train.CV[1,i]) | train.CV[1, i] <= 1) {
-    train.ss [i] <- NULL
+## Select (by elimination) columns where the CV is > 1 
+for (i in train.col:1) {
+  if(train.CV[1, i] <= avg.CV) {
+    train.filtered [i] <- NULL
   }
 }
 ##########
 ##Train models
-control.spec <- trainControl(method = 'cv', number = 10) 
-
 ## CART
-set.seed(2015)
-ss.model.rpart <- train(classe ~ ., 
-                        data = train.ss, 
-                        trControl = control.spec, 
-                        method = "rpart")
+model.cart <- TrainClasse('rpart')
 
 ## Random forest (randomForest package)
-ss.model.rf <- train(classe ~ ., 
-                     data = train.ss, 
-                     trControl = control.spec, 
-                     method = "rf")
+model.rf <- TrainClasse('rf')
 
 ## Stochastic Gradient Boosting
-ss.model.gbm <- train(classe ~ ., 
-                      data = train.ss, 
-                      trControl = control.spec, 
-                      method = "gbm",
-                      verbose = FALSE)
+model.gbm <- TrainClasse('gbm')
+
+#########
+## Download testing data
+test.url <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
+test.file <- "test.csv"
+if (!file.exists(test.file)) {
+  download.file(test.url, test.file, method = "curl")
+}
+
+## Process the data exactly as it was done for the training data
+test <- read.csv(test.file, na.strings = c("NA", "#DIV/0!"))
+test.filtered <- Filtered(test)
+
+## Find predictions and write to files for submission
+predict.test <- predict(ss.model.rf, test.ss)
